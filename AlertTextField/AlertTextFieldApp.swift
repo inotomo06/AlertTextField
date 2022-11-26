73d2cf3cde2da71a977d764d927ff51b007bb076
Inoue <21cm0106@jec.ac.jp>
2022-11-26 22:37:47 +0900

    Initial Commit

---
+++ AlertTextField/AlertTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertTextFieldApp.swift
+//  AlertTextField
+//
+//  Created by cmStudent on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct AlertTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
